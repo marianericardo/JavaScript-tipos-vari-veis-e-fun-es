dea8b9b0d3080c14c33ae41eaae1e4e8cd6e8687
marianericardo <163129300+marianericardo@users.noreply.github.com>
2024-06-07 12:01:43 -0300

    Update template-strings.js

--- template-strings.js
+++ template-strings.js
@@ -1,6 +1,6 @@
-const nome = "Ju";
-const idade = 2021-1981;
-const cidadeDeNascimento = "São Paulo";
+const nome = "mari";
+const idade = 2024-2006;
+const cidadeDeNascimento = "Parana";
 
 // const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
 
