2a35e2d5eb397eeefdd91d6565a57165a990413d
marianericardo <163129300+marianericardo@users.noreply.github.com>
2024-06-11 09:51:42 -0300

    Update conversoes.js

--- conversoes.js
+++ conversoes.js
@@ -2,13 +2,18 @@
 // booleanos 
 
 // conversão implícita
+// Acontece quando o javascript cnverte o numero em string
+// e depois compara
+// as 2 strings
 const numero = 456;
-const numeroString = Number("456a");
-
-//Number()
-//String()
-console.log(numero + numeroString)
+const numeroString = Number("456");
+console.log (numero === numeroString);
+console.log(numero + numeroString);
 
 // conversão explícita
+// Number
 
-console.warn(new Error('oi'))
+let novonumero = Number(numeroString);
+console.log(numero +novonumero);
+
+// String
